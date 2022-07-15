ff0534bbe81c3a573471a0920865ed2cc4d63113
Guy Marshall <guymarshall9@gmail.com>
2022-07-15 13:26:09 +0100

    Save number from input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+mod user_input;
+
 fn main() {
-    println!("Hello, world!");
+    let number: u128 = user_input::get_user_input("Enter a number:");
 }
